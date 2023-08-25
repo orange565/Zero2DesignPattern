980f68a8bc65207003b8564aa0810723fd24f48f
orange565 <makbule.demirel.06@gmail.com>
2023-08-25 13:57:40 +0000

    homework-1

--- makbule.py
+++ makbule.py
@@ -1,9 +1,45 @@
-"this is abc"
-def calculator(integer):
-    """Function printing python version."""
-    print(integer)
-    integer+=1
-    return integer
+"this is homework"
+#Görevler:
+#1-Aritmetik Operatörler
+x=7
+y=5
+print ("Number-1 is = ",x)
+print ("Number-2 is = ",y)
 
-OUTPUT = calculator(5)
-print(OUTPUT)
+#+	Addition	x + y
+result = x+y
+print("Result for Addition = " ,result)
+
+#-	Subtraction	x - y	
+result = x-y
+print("Result for Subtraction = " ,result)
+
+#*	Multiplication	x * y	
+result = x*y
+print("Result for Multiplication = " ,result)
+
+#/	Division	x / y	
+result = x/y
+print("Result for Division = " ,result)
+
+#%	Modulus	x % y	
+result = x%y
+print("Result for Modulus = " ,result)
+
+#**	Exponentiation	x ** y	
+result = x**y
+print("Result for Exponentiation = " ,result)
+
+#//	Floor division	x // y
+result = x//y
+print("Result for Floor division = " ,result)
+
+#Karşılaştırma Operatörleri
+
+#Mantıksal Operatörler
+
+#Atama Operatörleri
+
+#Üyelik ve Kimlik Operatörleri
+
+#Karşılaştırma ve Mantıksal Operatörlerin Kullanımı
