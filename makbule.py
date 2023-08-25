383bef70508a1f5ca82df7a534dc10f64e0cf0ce
orange565 <makbule.demirel.06@gmail.com>
2023-08-25 14:05:27 +0000

    homework update

--- makbule.py
+++ makbule.py
@@ -1,6 +1,6 @@
 "this is homework"
 #Görevler:
-#1-Aritmetik Operatörler
+#1-Python Arithmetic Operators
 x=7
 y=5
 print ("Number-1 is = ",x)
@@ -34,8 +34,38 @@ print("Result for Exponentiation = " ,result)
 result = x//y
 print("Result for Floor division = " ,result)
 
+###########################################################################
 #Karşılaştırma Operatörleri
 
+#==	Equal	x == y	
+if x==y:
+    print("Numbers are equal: ",x,"-",y)
+else:
+    print("Numbers are not equal: ",x,"-",y)
+
+#!=	Not equal	x != y	
+if x!=y:
+    print("Numbers are not equal: ",x,"-",y)
+else:
+    print("Numbers are equal: ",x,"-",y)
+
+#>	Greater than	x > y	
+if x > y:
+    print("Number-1 is greater than Number-2: ",x,"-",y)
+
+#<	Less than	x < y	
+if x < y:
+    print("Number-1 is less than Number-2: ",x,"-",y)
+
+#>=	Greater than or equal to	x >= y	
+if x >= y:
+    print("Number-1 is greater than Number-2 or equal: ",x,"-",y)
+
+#<=	Less than or equal to	x <= y
+if x <= y:
+    print("Number-1 is less than Number-2 or equal: ",x,"-",y)
+
+############################################################################
 #Mantıksal Operatörler
 
 #Atama Operatörleri
